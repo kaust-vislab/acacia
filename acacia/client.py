20ea58e091f865f9ba09b63dec6b3501a558477e
Christopher Knox <christopher.knox@kaust.edu.sa>
2012-09-30 14:16:52 +0300

    playing with python again

--- acacia/client.py
+++ acacia/client.py
@@ -15,28 +15,28 @@ class ReaderInterface(Thread):
     'handles all the clients communication with the supervisor and servers'
     def __init__(self, url, ctx):
         Thread.__init__(self)
-        self.readdir_rep = ctx.socket(zmq.REP)
-        self.readdir_rep.bind('inproc://readdir')
+        self.operation_rep = ctx.socket(zmq.REP)
+        self.operation_rep.bind('inproc://operation')
         self.dealer = ctx.socket(zmq.DEALER)
         self.dealer.connect(url)
 
     def run(self):
         'starts the thread'
         poller = zmq.Poller()
-        poller.register(self.readdir_rep, zmq.POLLIN)
+        poller.register(self.operation_rep, zmq.POLLIN)
         poller.register(self.dealer, zmq.POLLIN)
         while True:
             socks = dict(poller.poll())
-            if self.readdir_rep in socks:
-                if socks[self.readdir_rep] == zmq.POLLIN:
-                    query = self.readdir_rep.recv()
+            if self.operation_rep in socks:
+                if socks[self.operation_rep] == zmq.POLLIN:
+                    query = self.operation_rep.recv()
                     self.dealer.send(query)
             elif self.dealer in socks:
                 if socks[self.dealer] == zmq.POLLIN:
                     res = self.dealer.recv_json()
                     if res == {'shutdown': 'now'}:
                         break
-                    self.readdir_rep.send_json(res)
+                    self.operation_rep.send_json(res)
 
 
 class Reader(object):
@@ -44,16 +44,15 @@ class Reader(object):
         ctx = zmq.Context()
         self.interface = ReaderInterface(url, ctx)
         self.interface.start()
-        self.readdir_req = ctx.socket(zmq.REQ)
-        self.readdir_req.connect('inproc://readdir')
+        self.operation_req = ctx.socket(zmq.REQ)
+        self.operation_req.connect('inproc://operation')
 
-    def readdir(self):
+    def operation(self, command):
         ''' returns the contents of the directory
             currently only support a single flat directory
         '''
-        query = {'readdir': {'dir': '.'}}
-        self.readdir_req.send_json(query)
-        response = self.readdir_req.recv_json()
+        self.operation_req.send_json(command)
+        response = self.operation_req.recv_json()
         return response['result']
 
 
--- tests/test_client.py
+++ tests/test_client.py
@@ -7,27 +7,51 @@
 # =============================================================================
 
 from acacia import Reader
+from stat import S_IFREG
 from threading import Thread
 import zmq
+import random
 
 
-def dummy_readdir(desired):
+def dummy_supervisor(request, reply):
     ctx = zmq.Context()
     sock = ctx.socket(zmq.DEALER)
     sock.bind('tcp://*:6767')
     req = sock.recv_json()
-    assert req == {'readdir': {'dir': '.'}}
-    req['result'] = desired
+    assert req == request
+    req['result'] = reply
     sock.send_json(req)
     sock.send_json({'shutdown': 'now'})
 
 
-def test_readdir():
-    'tests to make sure that the readdir functionality works'
-    desired = ['.', '..', 'data.am']
-    rep_thread = Thread(target=dummy_readdir, args=(desired,))
+def operation_helper(request, reply):
+    rep_thread = Thread(target=dummy_supervisor, args=(request, reply,))
     rep_thread.start()
 
     acacia_reader = Reader('tcp://localhost:6767')
-    res = acacia_reader.readdir()
-    assert res == desired
+    res = acacia_reader.operation(request)
+    assert res == reply
+
+
+def test_readdir():
+    'tests to make sure that the readdir functionality works'
+    reply = ['.', '..', 'data.am']
+    request = {'readdir': {'path': '.'}}
+    operation_helper(request, reply)
+
+
+def test_getattr():
+    'ensures that getattr functionality works'
+    reply = dict(st_mode=(S_IFREG | 0444), st_size=1024)
+    request = {'getattr': {'path': 'data.am'}}
+    operation_helper(request, reply)
+
+
+def test_read():
+    tmp = range(64)
+    random.shuffle(tmp)
+    reply = bytearray(tmp)
+    request = {'read': {'path': 'data.am',
+                        'offset': 0,
+                        'size': 64}}
+    operation_helper(request, reply)
